f26596800d366884d265d9009de460a8bca22c97
Dan H. <goracle@users.noreply.github.com>
2025-12-23 20:26:41 -0800

    Auto commit.

--- search_common.py
+++ search_common.py
@@ -355,11 +355,6 @@ COEFFS_GENUS2 = [QQ(4), QQ(-4), QQ(-36), QQ(5), QQ(96), QQ(64)]
 DATA_PTS_GENUS2 = [QQ(-1)] # just the x values lol
 TERMINATE_WHEN_6 = 4
 
-# prestige curve lol, rank 4
-COEFFS_GENUS2 = [QQ(1), QQ(8), QQ(10), QQ(-10), QQ(-11), QQ(2), QQ(1)]
-DATA_PTS_GENUS2 = [QQ(-1)] # just the x values lol
-TERMINATE_WHEN_6 = 11
-
 # $y^2 = 4x^6 - 12x^5 + 16x^4 - 8x^3 - 3x^2 + 4x$ # rank 2
 COEFFS_GENUS2 = [QQ(4), QQ(-12), QQ(16), QQ(-8), QQ(-3), QQ(4), QQ(0)] # rank 2
 DATA_PTS_GENUS2 = [QQ(1)] # just the x values lol
@@ -369,6 +364,11 @@ TERMINATE_WHEN_6 = 3
 COEFFS_GENUS2 = [QQ(4), QQ(0), QQ(9), QQ(-4), QQ(2), QQ(-4), QQ(1)] # rank 2
 DATA_PTS_GENUS2 = [QQ(0)] # just the x values lol
 TERMINATE_WHEN_6 = 3
+
+# prestige curve lol, rank 4
+COEFFS_GENUS2 = [QQ(1), QQ(8), QQ(10), QQ(-10), QQ(-11), QQ(2), QQ(1)]
+DATA_PTS_GENUS2 = [QQ(-1)] # just the x values lol
+TERMINATE_WHEN_6 = 11
 
 COEFFS_GENUS2 = [QQ(1), QQ(-12), QQ(30), QQ(2), QQ(-15), QQ(2), QQ(1)] # rank 4
 DATA_PTS_GENUS2 = [QQ(1)] # just the x values lol
